2e230a28083b4338c8be2eef39e21c1a9b6f9beb
hurtsky <sabmar_com@msn.com>
2013-07-30 16:01:38 -0700

    Frandom Hack - Credits to Christopher
    Frandom support on boot

--- sbin/ext/hurtsky.sh
+++ sbin/ext/hurtsky.sh
@@ -10,7 +10,29 @@ echo "Mount system for read and write"
 mount -o rw,remount /system
 echo
 
-# Adding Scripts
+#
+# Fast Random Generator (frandom) support at boot
+#
+if [ -f "/lib/modules/frandom.ko" ]; then
+	# Load frandom module if not built inside the zImage
+	echo "Fast Random Generator (frandom): Loading module..."
+	insmod /lib/modules/frandom.ko
+fi
+if [ -c "/dev/frandom" ]; then
+	# Redirect random and urandom generation to frandom char device
+	echo "Fast Random Generator (frandom): Initializing..."
+	rm -f /dev/random
+	rm -f /dev/urandom
+	ln /dev/frandom /dev/random
+	ln /dev/frandom /dev/urandom
+	chmod 0666 /dev/random
+	chmod 0666 /dev/urandom
+	echo "Fast Random Generator (frandom): Ready!"
+	echo
+else
+	echo "Fast Random Generator (frandom): Not supported!"
+	echo
+fi
 
 # Mount system read-only
 echo "Mount system read-only"
